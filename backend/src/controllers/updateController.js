347c068079211894369f89e9d740bc37730a3923
matheus kousen <matheuskousen@.com>
2020-04-17 00:16:57 -0300

    setting updates

--- backend/src/controllers/updateController.js
+++ backend/src/controllers/updateController.js
@@ -7,7 +7,12 @@ module.exports = {
   },
   async create(request, response) {
     const id = crypto.randomBytes(4).toString("HEX");
-    const { originalname: name, size, key, location: url = "" } = request.file;
+    const {
+      originalname: name,
+      size,
+      key,
+      location: url = `../../assets/${key}`,
+    } = request.file;
     const post = { name, size, key, url, id };
     await connection("urls").insert(post);
     return response.json(post);
